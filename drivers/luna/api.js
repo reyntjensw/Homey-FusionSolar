5bcf27740293d7f554a84fd297ac3a5c18beef62
Wouter Reyntjens <wouter.reyntjens@be.sentia.com>
2022-04-27 15:43:49 +0200

    Adding error catching

--- drivers/luna/api.js
+++ drivers/luna/api.js
@@ -10,115 +10,146 @@ class LunaApi {
     }
 
     async initializeSession() {
-        const accountUrl = `${baseUrl}/login`;
+        try {
+            const accountUrl = `${baseUrl}/login`;
 
-        const requestBody = JSON.stringify({
-            "userName": this.username,
-            "systemCode": this.password
-        });
-        const apiData = await this.apiRequest(accountUrl, 'POST', requestBody);
-        return apiData;
+            const requestBody = JSON.stringify({
+                "userName": this.username,
+                "systemCode": this.password
+            });
+            const apiData = await this.apiRequest(accountUrl, 'POST', requestBody);
+            return apiData;
+        } catch (error) {
+            this.error(`Unavailable (${error})`);
+        }
+
     }
 
     async apiRequest(url, methodContent, requestBody) {
-        const apiResponse = await fetch(url, {
-            method: methodContent,
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json;charset=UTF-8'
-            },
-            body: requestBody,
-        });
+        try {
+            const apiResponse = await fetch(url, {
+                method: methodContent,
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json;charset=UTF-8'
+                },
+                body: requestBody,
+            });
 
-        const apiData = await apiResponse;
-        const bodyData = await apiData.text();
+            const apiData = await apiResponse;
+            const bodyData = await apiData.text();
 
-        token = apiData.headers.get('xsrf-token');
-        if (apiData.statusText === 'OK' && bodyData.length > 0) {
-            return true;
-        } else {
-            return false;
+            token = apiData.headers.get('xsrf-token');
+            if (apiData.statusText === 'OK' && bodyData.length > 0) {
+                return true;
+            } else {
+                return false;
+            }
+
+        } catch (error) {
+            this.error(`Unavailable (${error})`);
+            throw new Error(apiData.text());
         }
-        throw new Error(apiData.text());
+
     }
 
     async getSystems() {
-        const systemsUrl = `${baseUrl}/getStationList`;
+        try {
+            const systemsUrl = `${baseUrl}/getStationList`;
 
-        const response = await fetch(systemsUrl, {
-            method: "POST",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json;charset=UTF-8',
-                "XSRF-TOKEN": token,
-            }
-        });
-        const apiData = await response.json();
-        return apiData["data"];
+            const response = await fetch(systemsUrl, {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json;charset=UTF-8',
+                    "XSRF-TOKEN": token,
+                }
+            });
+            const apiData = await response.json();
+            return apiData["data"];
+        } catch (error) {
+            this.error(`Unavailable (${error})`);
+        }
+
     }
 
     async getBasicStats(stationCode) {
-        const systemsUrl = `${baseUrl}/getStationRealKpi`;
+        try {
+            const systemsUrl = `${baseUrl}/getStationRealKpi`;
 
-        let bodyData = JSON.stringify({
-            "stationCodes": stationCode
-        });
-        const response = await fetch(systemsUrl, {
-            method: "POST",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json;charset=UTF-8',
-                "XSRF-TOKEN": token,
-            },
-            body: bodyData
-        });
+            let bodyData = JSON.stringify({
+                "stationCodes": stationCode
+            });
+            const response = await fetch(systemsUrl, {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json;charset=UTF-8',
+                    "XSRF-TOKEN": token,
+                },
+                body: bodyData
+            });
 
-        const apiData = await response.json();
+            const apiData = await response.json();
 
-        return apiData["data"][0]["dataItemMap"];
+            return apiData["data"][0]["dataItemMap"];
+        } catch (error) {
+            this.error(`Unavailable (${error})`);
+        }
+
     }
     async getDevList(stationCode, devTypeId) {
-        const systemsUrl = `${baseUrl}/getDevList`;
+        try {
+            const systemsUrl = `${baseUrl}/getDevList`;
 
-        let bodyData = JSON.stringify({
-            "stationCodes": stationCode
-        });
-        const response = await fetch(systemsUrl, {
-            method: "POST",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json;charset=UTF-8',
-                "XSRF-TOKEN": token,
-            },
-            body: bodyData
-        });
+            let bodyData = JSON.stringify({
+                "stationCodes": stationCode
+            });
+            const response = await fetch(systemsUrl, {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json;charset=UTF-8',
+                    "XSRF-TOKEN": token,
+                },
+                body: bodyData
+            });
 
-        const apiData = await response.json();
+            const apiData = await response.json();
 
-        for (let index = 0; index < apiData["data"].length; index++) {
-            if (apiData["data"][index]["devTypeId"] == devTypeId) {
-                return apiData["data"][index];
+            for (let index = 0; index < apiData["data"].length; index++) {
+                if (apiData["data"][index]["devTypeId"] == devTypeId) {
+                    return apiData["data"][index];
+                }
             }
+        } catch (error) {
+            this.error(`Unavailable (${error})`);
         }
+
     }
     async getDevRealKpi(devIds, devTypeId) {
-        const systemsUrl = `${baseUrl}/getDevRealKpi`;
-        let bodyData = JSON.stringify({
-            "devIds": devIds,
-            "devTypeId": devTypeId
-        });
-        const response = await fetch(systemsUrl, {
-            method: "POST",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json;charset=UTF-8',
-                "XSRF-TOKEN": token,
-            },
-            body: bodyData
-        });
+        try {
+            const systemsUrl = `${baseUrl}/getDevRealKpi`;
+            let bodyData = JSON.stringify({
+                "devIds": devIds,
+                "devTypeId": devTypeId
+            });
+            const response = await fetch(systemsUrl, {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json;charset=UTF-8',
+                    "XSRF-TOKEN": token,
+                },
+                body: bodyData
+            });
 
-        const apiData = await response.json();
-        return apiData["data"];
+            const apiData = await response.json();
+            return apiData["data"];
+        } catch (error) {
+            this.error(`Unavailable (${error})`);
+        }
+
     }
 }
 
